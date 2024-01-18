3e0eb182b2a6018eae10129f2926e6fb66a6af52
Spencer <spencer@bookservo.com>
2024-01-18 14:10:52 -0500

    scheduling blocking

--- frontend/src/components/AppointmentScheduling/index.js
+++ frontend/src/components/AppointmentScheduling/index.js
@@ -1,42 +1,37 @@
 import { useState } from 'react';
 import './AppointmentScheduling.css'
 import DatePicker from 'react-datepicker';
-import { addDays, subDays } from 'date-fns';
+import { addDays, format, subDays } from 'date-fns';
 
 const AppointmentScheduling = ({schedulingOpen}) => {
-    const [startDate, setStartDate] = useState(new Date());
+    const [startDate, setStartDate] = useState(addDays(new Date(), 2));
+    let formattedDate = format(startDate, "M/d/yy @h:mmaaa")
 
     const handleDateChange = (date) => {
         setStartDate(date);
     };
     return (
         <div className={`appointment-scheduling ${schedulingOpen ? '' : 'minimize' }`}>
+            {/* <h3>Instant Scheduling</h3> */}
+            <div className="scheduling-output">
+                {format(startDate, "EEEE, MMMM do @h:mmaaa")}
+            </div>
             <DatePicker
-                // inline
+                inline
                 selected={startDate}
                 onChange={handleDateChange}
-                // showTimeSelect
-                // dateFormat="Pp"
-                showIcon
-                toggleCalendarOnIconClick
-                icon={
-                    <svg xmlns="http://www.w3.org/2000/svg" 
-                    height="24" 
-                    viewBox="0 -960 960 960" 
-                    width="24">
-                        <path d="M200-80q-33 0-56.5-23.5T120-160v-560q0-33 23.5-56.5T200-800h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-720v560q0 33-23.5 56.5T760-80H200Zm0-80h560v-400H200v400Zm0-480h560v-80H200v80Zm0 0v-80 80Z"/>
-                    </svg>
-                }
-                isClearable
+                showTimeSelect
+                dateFormat="Pp"
                 placeholderText="Select a Date"
                 minTime={new Date().setHours(9, 0, 0)}
                 maxTime={new Date().setHours(17, 0, 0)}
                 timeIntervals={120}
                 highlightDates={[addDays(new Date(), 8)]}
-                minDate={ new Date()}
+                minDate={ addDays(new Date(), 2)}
                 maxDate={ addDays(new Date(), 545)}
                 monthsShown={2}
             />
+            <button className="scheduling-continue-button">Continue - {formattedDate}</button>
         </div>
     )
 }
