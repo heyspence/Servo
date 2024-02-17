791d37ca05fb0e8b978a17dccf9225fce71efee9
Spencer <spencer@bookservo.com>
2024-02-16 22:31:28 -0500

    prevent mailing to demo emails

--- backend/app/mailers/user_mailer.rb
+++ backend/app/mailers/user_mailer.rb
@@ -1,5 +1,6 @@
 class UserMailer < ApplicationMailer
     def welcome_email(user)
+        return if user.email.ends_with?('dashdoor.com')
         @user = user
         @url = "https://www.bookservo.com/"
         mail(to: @user.email, subject: 'Welcome to Servo')
