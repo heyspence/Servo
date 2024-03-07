a597324a9b909eb11f9d27a5d42840ebe631c4de
Spencer <spencer@bookservo.com>
2024-03-07 18:12:12 -0500

    migration fix

--- backend/db/migrate/20240307193057_create_calendars_for_vendors_without_one.rb
+++ backend/db/migrate/20240307193057_create_calendars_for_vendors_without_one.rb
@@ -1,12 +1,14 @@
 class CreateCalendarsForVendorsWithoutOne < ActiveRecord::Migration[7.0]
-  def up 
+  def up
     Vendor.find_each do |vendor|
       calendar = vendor.vendor_calendar
-      unless calendar 
-        VendorCalendar.create!({vendor_id: vendor.id})
+
+      unless calendar
+        calendar = VendorCalendar.create!(vendor_id: vendor.id)
       end
 
-      if calendar[:access_token].present?
+      # Now, `calendar` is guaranteed to not be nil, so this check is safe
+      if calendar.access_token.present?
         calendar.update!(api_integrated: true)
       end
     end
