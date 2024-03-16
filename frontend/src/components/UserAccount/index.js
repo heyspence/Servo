dcea51a5493084ce1866d697740e86566ddd69a1
claudiaaziz <claudiahannahh@gmail.com>
2024-03-15 21:06:45 -0400

    Updated user persisting to backend

--- frontend/src/components/UserAccount/index.js
+++ frontend/src/components/UserAccount/index.js
@@ -1,63 +1,79 @@
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
-import './UserAccount.css'
-import { useSelector } from 'react-redux';
-import { isLoggedIn } from '../store/session';
-import { useState } from 'react';
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import "./UserAccount.css";
+import { useDispatch, useSelector } from "react-redux";
+import { isLoggedIn } from "../store/session";
+import { useState } from "react";
+import { updateUser } from "../store/users";
 
 const UserAccount = () => {
-    const {user} = useSelector(state => {
-        return ({
-            user: state.session?.user ? state.session.user : {}
-        })
-    });
-    const history = useHistory();
-    const [accountFormValues, setAccountFormValues] = useState({...user});
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => {
+    return {
+      user: state.session?.user ? state.session.user : {},
+    };
+  });
+  const history = useHistory();
+  const [accountFormValues, setAccountFormValues] = useState({ ...user });
 
-    if(!user){
-        history.push('/')
-    }
+  if (!user) history.push("/");
 
-    return (
-        <div className="user-account">
-            <div className="user-account-backdrop">
-                <h1>Account Settings</h1>
-                <form>
-                    <input type="text" 
-                            value={accountFormValues.firstName} 
-                            onChange={(e) => {
-                                setAccountFormValues(prevValues => (
-                                {...prevValues, firstName: e.target.value}
-                                ))
-                            }}
-                    />
-                    <input type="text" 
-                            value={accountFormValues.lastName} 
-                            onChange={(e) => {
-                                setAccountFormValues(prevValues => (
-                                {...prevValues, lastName: e.target.value}
-                                ))
-                            }}
-                    />
-                    <input type="text" 
-                            value={accountFormValues.phoneNumber} 
-                            onChange={(e) => {
-                                setAccountFormValues(prevValues => (
-                                {...prevValues, phoneNumber: e.target.value}
-                                ))
-                            }}
-                    />
-                    <input type="text" 
-                            value={accountFormValues.email} 
-                            onChange={(e) => {
-                                setAccountFormValues(prevValues => (
-                                {...prevValues, email: e.target.value}
-                                ))
-                            }}
-                    />
-                </form>
-            </div>
-        </div>
-    )
-}
+  const handleUpdateUser = () => {
+    const { id, firstName, lastName, email, phoneNumber } = accountFormValues;
+    const updatedUserData = { id, first_name: firstName, last_name: lastName, email, phone_number: phoneNumber }; // temp since snake_case_params isnt working on backend
+    // debugger;
+    dispatch(updateUser(updatedUserData));
+  };
+
+  return (
+    <div className="user-account">
+      <div className="user-account-backdrop">
+        <h1>Account Settings</h1>
+        <form onSubmit={handleUpdateUser}>
+          <input
+            type="text"
+            value={accountFormValues.firstName}
+            onChange={(e) => {
+              setAccountFormValues((prevValues) => ({
+                ...prevValues,
+                firstName: e.target.value,
+              }));
+            }}
+          />
+          <input
+            type="text"
+            value={accountFormValues.lastName}
+            onChange={(e) => {
+              setAccountFormValues((prevValues) => ({
+                ...prevValues,
+                lastName: e.target.value,
+              }));
+            }}
+          />
+          <input
+            type="text"
+            value={accountFormValues.phoneNumber}
+            onChange={(e) => {
+              setAccountFormValues((prevValues) => ({
+                ...prevValues,
+                phoneNumber: e.target.value,
+              }));
+            }}
+          />
+          <input
+            type="text"
+            value={accountFormValues.email}
+            onChange={(e) => {
+              setAccountFormValues((prevValues) => ({
+                ...prevValues,
+                email: e.target.value,
+              }));
+            }}
+          />
+          <button type="submit">Save</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default UserAccount;
--- frontend/src/components/store/users.js
+++ frontend/src/components/store/users.js
@@ -8,12 +8,17 @@ export const receiveUser = (user) => ({
 });
 
 export const updateUser = (userData) => async (dispatch) => {
-  const res = await csrfFetch("/api/benches", {
-    method: "POST",
-    body: userData,
+  console.log("🦋🦋🦋 ~ userData:", userData);
+  // debugger;
+  const res = await csrfFetch(`/api/users/${userData.id}`, {
+    method: "PATCH",
+    body: JSON.stringify(userData),
   });
 
   const updatedUser = await res.json();
+  console.log('🦋🦋🦋 ~ res:', res);
+  console.log('🦋🦋🦋 ~ updatedUser:', updatedUser);
+  debugger
   dispatch(receiveUser(updatedUser));
   return res;
 };
