d56b675fdb11d19783b827e6598ec3211d4c17a9
Spencer <spencer@bookservo.com>
2024-03-24 01:07:58 -0400

    Remove create order endpoint, add Stripe webhook for order creation

--- frontend/src/components/store/bookings.js
+++ frontend/src/components/store/bookings.js
@@ -4,8 +4,6 @@ const RECEIVE_BOOKING = 'booking/RECEIVE_BOOKING'
 const REMOVE_BOOKING = 'booking/REMOVE_BOOKING'
 const RECEIVE_BOOKINGS = 'booking/RECEIVE_BOOKINGS'
 const REMOVE_BOOKINGS = 'booking/REMOVE_BOOKINGS'
-// const TOGGLE_BOOKING = 'booking/TOGGLE_BOOKING'
-// const ASSIGN_BOOKING = 'booking/ASSIGN_BOOKING'
 
 const receiveBooking = booking => ({
     type: RECEIVE_BOOKING,
@@ -25,25 +23,6 @@ const removeBooking = bookingId => ({
 export const removeBookings = () => ({
     type: REMOVE_BOOKINGS
 })
-
-// export const toggleCart = () => ({
-//     type: TOGGLE_CART
-// })
-
-// export const assignCart = (vendor) => ({
-//     type: ASSIGN_CART,
-//     vendor
-// })
-
-export const deleteBookings = userId => async dispatch => {
-    const res = await csrfFetch(`/api/user/${userId}/bookings/destroy_all`,{
-        method: 'DELETE'
-    })
-
-    if(res.ok){
-        dispatch(removeBookings())
-    }
-}
 
 export const createBooking = booking => async dispatch =>{
     try{
@@ -90,7 +69,7 @@ export const updateBooking = booking => async dispatch => {
 }
 
 export const createOrder = booking => async dispatch => {
-    const res = await csrfFetch(`/api/bookings/${booking.booking.id}/create-order`,{
+    const res = await csrfFetch(`/api/orders/${booking.booking.id}/create-order`,{
         method: 'PATCH',
         body: JSON.stringify(booking)
     })
@@ -112,9 +91,6 @@ const bookingsReducer = (state = {}, action) => {
         case(REMOVE_BOOKING):
             delete newState.bookings[action.bookingId];
             return newState;
-        case(REMOVE_BOOKINGS):
-            newState.bookings = {}
-            return newState
         default:
             return newState
     }
